d79bf7a9852a0e547987e98e87b93c69e1e4c927
Mathieu Tuli <tuli.mathieu@gmail.com>
2022-07-22 07:56:09 -0400

    Add changes from a while

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -5,6 +5,7 @@ config.TerminalInteractiveShell.confirm_exit = False
 config.TerminalInteractiveShell.editing_mode = 'vi'
 config.TerminalInteractiveShell.highlighting_style = 'monokai'  # or emacs
 config.TerminalInteractiveShell.true_color = True
+config.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
 
 # --nosep
 config.InteractiveShell.separate_in = ''
