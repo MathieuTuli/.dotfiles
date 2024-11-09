a6d22aa6d6670537aa760e1e11a9d954dbb29e51
Mathieu Tuli <tuli.mathieu@gmail.com>
2024-11-09 15:58:06 -0500

    Updates

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,3 +1,4 @@
 c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
 c.TerminalInteractiveShell.timeoutlen = 0.25
+c.TerminalInteractiveShell.confirm_exit = False
